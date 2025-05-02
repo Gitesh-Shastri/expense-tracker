ee3da201a91e5f3ee68536598faecee3772e594e
Gitesh-Shastri <giteshshastri100@gmail.com>
2025-05-02 14:02:49 +0530

    feat: Add broker and investment type management templates with CRUD functionality

--- app/expensetracker/settings.py
+++ app/expensetracker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expensetracker',
     'expenses',
     'emi_app',
     'investment_app',
